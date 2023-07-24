3ee39dfcaf954ca3399cc5a4e5d333da7d84f15a
Nikolai <nikolai.ivanov3110@gmail.com>
2023-07-24 11:40:11 +0400

    feat: add createUser Mutation

---
+++ src/routes/graphql/users/userMutation.ts
@@ -0,0 +1,40 @@
+import { GraphQLNonNull, GraphQLObjectType } from "graphql";
+import { UserType } from "./typeUser.js";
+import { CreateUserInput } from "./InputUser.js";
+import { Context } from "../types/context.js";
+import { UUIDType } from '../types/uuid.js';
+
+export interface ICreateUserInput {
+    user: {
+      name: string;
+      balance: number;
+    };
+  }
+
+export const UserMutation = {
+    createUser: {
+        type: UserType as GraphQLObjectType,
+        args: {
+            user: {type: new GraphQLNonNull(CreateUserInput)},
+        },
+        resolve: async (__: unknown, {user}: ICreateUserInput, {prisma}: Context) => await prisma.user.create({data: user})
+    },
+    updateUser: {
+        type: UserType as GraphQLObjectType,
+        args: {
+            id: {type: new GraphQLNonNull(UUIDType)},
+            user: {type: new GraphQLNonNull(CreateUserInput)},
+        },
+        resolve: async (__: unknown, {id, user}:{ id: string, user: ICreateUserInput }, { prisma }: Context) => 
+            await prisma.user.update({where: {id}, data: user})
+    },
+
+    deleteUser:  {
+        type: UserType as GraphQLObjectType,
+        args:{
+            id: {type: new GraphQLNonNull(UUIDType)},
+        },
+        resolve: async (__: unknown, { id }: { id: string }, { prisma }: Context) => 
+            await prisma.user.delete({where: {id} })
+    }
+};
