1e007573be402e9799d592d2cea3b5447aead16f
Nikolai <nikolai.ivanov3110@gmail.com>
2023-07-24 19:39:29 +0400

    feat: add dataloaders

--- src/routes/graphql/types/context.ts
+++ src/routes/graphql/types/context.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '@prisma/client';
+import { MemberType, Post, Profile, User } from '@prisma/client';
+import DataLoader from 'dataloader';
+
+export interface IDataLoaders {
+  postDataLoader: DataLoader<string, Post[] | undefined>;
+  memberDataLoader: DataLoader<string, MemberType | undefined>;
+  profileDataLoader: DataLoader<string, Profile | undefined>;
+  userDataLoader: DataLoader<string, User | undefined>;
+}
 
 export interface Context {
   prisma: PrismaClient;
+  loaders: IDataLoaders;
 }
